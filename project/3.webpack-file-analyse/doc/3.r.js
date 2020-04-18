f8b1d7f079fb48b2a00c4132b57452595a62ff7d
wjq1994 <1194497392@qq.com>
2020-04-18 20:35:57 +0800

    新增webpack的文件分析

---
+++ project/3.webpack-file-analyse/doc/3.r.js
@@ -0,0 +1,13 @@
+//转为ES6模式
+function r(obj) {
+    Object.defineProperty(obj, Symbol.toStringTag, {
+        enumerable: true,
+        value: 'Module'
+    });
+    obj[Symbol.toStringTag] = 'Module';
+    Object.defineProperty(obj, '__esModule', { enumerable: true, value: true });
+    obj['__esModule'] = true;
+}
+let obj = {};
+r(obj);
+console.log(obj);
